f01dbdee35bc703980dc55e7f99c2090ca97a233
cleiv-ing <cheyenneleiv@cpp.edu>
2025-01-30 15:10:59 -0800

    Update Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,14 @@
+/*
+    Quiz for setting up GitHub and VS Code
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    printf("Hello World again!");
+    return 0;
+}
