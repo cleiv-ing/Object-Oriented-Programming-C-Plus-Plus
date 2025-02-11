eeb106cf5af987a9d53269cf8acac2637b01e967
cleiv-ing <cheyenneleiv@cpp.edu>
2025-02-11 11:18:32 -0800

    Completed array.cpp
    The file doesn't exist, trying to fix.

--- Lab2/array.cpp
+++ Lab2/array.cpp
@@ -13,3 +13,29 @@ void printMemArr(const int * arr, int size){
     for(int i = 0; i<size; i++)
         printf("Value :%i at Memory Location %p\n", arr[i], arr + i);
 }
+
+/*
+@ brief increments all of the elements in array by 10
+@ param arr - array of integers
+@ param size - the number of elements in the array
+*/
+
+void incArrBy10(int * arr, int size){ // void incArryBy10 (int arr[], int size)
+    for(int i = 0; i<size; i++){
+        arr[i] += 10;
+    }
+}
+
+int main() {
+    const int SIZE = 5;
+    int arr[SIZE];
+    for(int i = 0; i<SIZE; i++){
+        arr[i] = 100 + i;
+    }
+    printf("Before-------\n");
+    printMemArr(arr,SIZE);
+    incArrBy10(arr,SIZE);
+    printf("After--------\n");
+    printMemArr(arr,SIZE);
+    return 0;
+}
