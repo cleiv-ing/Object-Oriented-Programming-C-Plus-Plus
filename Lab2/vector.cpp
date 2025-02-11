842c7ec7c0471f55fed3ac09ca123290522a3acf
cleiv-ing <cheyenneleiv@cpp.edu>
2025-02-11 14:08:47 -0800

    Started vector.cpp
    Changing everything from array to vector.

---
+++ Lab2/vector.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+@brief prints the elements in the vector and their memory locations
+@ param v - vector of integers
+
+*/
+
+void printMemVec(int* v, int size){
+
+
+}
+
+void incVecBy10(int* v, int SIZE){
+for (int i = 0, i<=10, i++)
+
+}
+
+int main(){
+
+    int SIZE = 5;
+    vector<int> vec(5);
+
+    for (int i = 100; i<=104; i++){
+        vec[i] = i + 1;
+    }
+
+    printf("Before Increment------\n");
+    printMemVec(vec, SIZE);
+}
