24cd7069183f38c15d6785ee221ae704767f7e78
cleiv-ing <cheyenneleiv@cpp.edu>
2025-02-18 00:33:18 -0800

    Finished vector and array

--- Array_Vector/array_vec.cpp
+++ Array_Vector/array_vec.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+void printString(string *arrStr, int sizeStr){
+    printf("Each string is worth %lu bytes\n", sizeof(arrStr[0]));
+    for(int p = 0; p< sizeStr; p++){
+    printf("Value:%s at Memory Location: %p\n", arrStr[p].c_str(), arrStr+p);
+}
+}
+
 
 void printMem(int * arr, int size){ //void printMem(int arr[], int size)
     printf("Each int is worth %lu bytes\n", sizeof(arr[0]));
@@ -11,8 +20,6 @@ void printMem(int * arr, int size){ //void printMem(int arr[], int size)
     }
 
 }
-
-
 
 int main(){
     const int size = 5;
@@ -30,8 +37,20 @@ int main(){
     printf("The address for %i is Memory Location: %p\n", arr[0], arr+0);
     printf("The address for %i is Memory Location: %p\n", arr[1], arr+1);
 
-    printf("The subtraction of these two addresses is %p\n", arr[0]-arr[1],arr+0-arr+1);
-    
-    
+//new string array
+    const int sizeStr = 5;
+    string arrStr[sizeStr] = {"Bonjour", "Kamusta", "Hola", "Ciao", "Hallo"};
+    printString(arrStr, size);
+
+//new vector
+    vector <double> vec = {1,2,3,4,5};
+
+    vec.push_back(6);
+    vec.push_back(7);
+
+    cout << "The number of elements in the vector is " << vec.size() << "\n";
+    vec.pop_back();
+    cout << "The new number of elements in the vector is " << vec.size() << "\n";
+
     return 0;
 }
